7b3ba219e03087a62fd42ba3a149450581cdc4d7
Mougani <pmougani16@gmail.com>
2023-12-08 12:42:36 +0100

    origin

---
+++ TP02.py
@@ -0,0 +1,23 @@
+age = 42
+nom = 3.14
+valeur = "Bonjour tout le monde"
+Bool = True
+
+
+
+print(age, type(age))
+print(nom, type(nom))
+print(valeur, type(valeur))
+print(Bool, type(Bool))
+
+
+age = 42
+nom = 3.14
+valeur = "Bonjour tout le monde"
+Bool = True
+
+# Affichage de la valeur avec le type associé sur la même ligne
+print(f"Age: {age} | Type: {type(age)}")
+print(f"Nom: {nom} | Type: {type(nom)}")
+print(f"Valeur: {valeur} | Type: {type(valeur)}")
+print(f"Booléen: {Bool} | Type: {type(Bool)}")
